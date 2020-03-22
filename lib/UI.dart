958a15d6abe55f386df92082fc12a7437c5a16e2
Achinths Isuru <achinthaisuru444@gmail.com>
2020-03-22 17:15:14 +0530

    UI interface design finished

--- lib/UI.dart
+++ lib/UI.dart
@@ -6,10 +6,68 @@ class UI extends StatefulWidget {
 }
 
 class _UIState extends State<UI> {
+
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('NOVEL COVID API'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 30,
+                  bottom: 15,
+                  left: 5,
+                  right: 5,
+                ),
+                child: TextField(
+                  decoration: InputDecoration(border: OutlineInputBorder()),
+                ),
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: 5,
+                        right: 5,
+                      ),
+                      child: RaisedButton(
+                        child: Text('Search'),
+                        onPressed: () {},
+                        color: Colors.lightBlue,
+                        textColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: 5,
+                        right: 5,
+                      ),
+                      child: RaisedButton(
+                        child: Text('All Information'),
+                        textColor: Colors.white,
+                        onPressed: () {},
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
