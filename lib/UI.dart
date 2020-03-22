3d0c7af7178cebc5f816bb74413da8b14f1b1946
Achinths Isuru <achinthaisuru444@gmail.com>
2020-03-22 16:57:24 +0530

    created UI page

---
+++ lib/UI.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UI extends StatefulWidget {
+  @override
+  _UIState createState() => _UIState();
+}
+
+class _UIState extends State<UI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
