42ffbf4cf241a95be9fcb5dd6d90471e582d36fd
Achinths Isuru <achinthaisuru444@gmail.com>
2020-03-22 18:23:48 +0530

    comments added

--- lib/covid_info.dart
+++ lib/covid_info.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-Future<Map<String, dynamic>> allCovidInfo(http.Client httpClient) async {
+/// This method will return the total statistics
+Future<Map<String, dynamic>> allCovidInfo(http.Client httpClient) async { 
   final httpResponse = await httpClient.get('https://corona.lmao.ninja/all');
   if (httpResponse.statusCode == 200) {
     final jsonBody = json.decode(httpResponse.body);
@@ -12,6 +13,8 @@ Future<Map<String, dynamic>> allCovidInfo(http.Client httpClient) async {
   }
 }
 
+
+/// This method will return the statistics based on the country
 Future<Map<String, dynamic>> countryCovidInfo(String country,http.Client httpClient) async {
   final httpResponse = await httpClient.get('https://corona.lmao.ninja/countries/$country');
   if (httpResponse.statusCode == 200) {
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:novel_covid_api/covid_info.dart';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  final String country = 'Sri Lanka';
+  final String country = 'Sri Lanka'; // Enter a Country
   final resposeAll = await allCovidInfo(http.Client());
   final responseCountry = await countryCovidInfo(country, http.Client());
 
